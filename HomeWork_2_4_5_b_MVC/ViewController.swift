ad2cd4069f6bf1f380ae39bc0a2e22c085a06706
Vonckad <Vonkad@yandex.ru>
2021-02-19 10:57:50 +0300

    Initial Commit

---
+++ HomeWork_2_4_5_b_MVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork_2_4_5_b_MVC
+//
+//  Created by Vlad Ralovich on 2/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
